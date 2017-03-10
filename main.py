ad6f6c7ac152255912faa7f2c8a957f226408576
davidallison <david_mark_allison@yahoo.co.uk>
2017-03-10 11:29:31 +0000

    Committing some initial changes to main. Still need to add keys/tokens via CLI arguments.

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import tweepy
+import os
 
 sim_file = open("simdog.txt", "r")
 simdog = sim_file.read()
 
 sim_arr = simdog.split(".")
 
-print(len(sim_arr))
+print(sim_arr[1])
+
+# # Consumer keys and access tokens, used for OAuth
+# consumer_key = ''
+# consumer_secret = ''
+# access_token = ''
+# access_token_secret = ''
+#
+# # OAuth process, using the keys and tokens
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+#
+# # Creation of the actual interface, using authentication
+# api = tweepy.API(auth)
+#
+# tweet = ""
+# # Sample method, used to update a status
+# if len(tweet) < 140 || len(tweet) > 0:
+#     api.update_status(tweet)
